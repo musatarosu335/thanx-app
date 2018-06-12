938ba332f345043eaa5ddc73c90310e810908cc3
Kentaro Sakae <musamusa0607@gmail.com>
2018-06-12 23:19:23 +0900

    Firebaseセットアップ

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+// import 'firebase/auth';
 import React from 'react';
 import ReactDom from 'react-dom';
+import ENV from '../env.json';
+
 import App from './components/App';
+
+// Firebase setting
+const config = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PRJ_ID,
+  storageBucket: ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
+};
+firebase.initializeApp(config);
+// Enables the use of Timestamps for timestamp fields in DocumentSnapshots.
+const firestore = firebase.firestore();
+const settings = { timestampsInSnapshots: true };
+firestore.settings(settings);
 
 ReactDom.render(
   <App />,
