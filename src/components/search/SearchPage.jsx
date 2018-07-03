edcd9daa3004536060b46bcc583094683e99b1b0
Kentaro Sakae <musamusa0607@gmail.com>
2018-07-04 00:07:43 +0900

    Material-uiのtabのテスト

--- src/components/search/SearchPage.jsx
+++ src/components/search/SearchPage.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { lifecycle } from 'recompose';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
 
 import FollowerNotification from '../../containers/search/FollowerNotification';
 import SearchForm from '../../containers/search/SearchForm';
@@ -16,6 +21,7 @@ const Container = styled.div`
   padding: 0 10px;
 `;
 
+/*
 const SearchPage = ({ followingUser }) => (
   // pairがいる場合はMyPageへリダイレクトさせる必要あり
   <Container>
@@ -29,11 +35,76 @@ const SearchPage = ({ followingUser }) => (
     }
   </Container>
 );
+*/
 
+const TabContainer = (props) => {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+};
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
+class SearchPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    // フォローしているユーザの取得
+    this.props.fetchAndSetFollowingUser();
+    // フォロワー一覧の取得
+    this.props.fetchAndSetFollower();
+  }
+
+  handleChange(event, value) {
+    this.setState({ value });
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <Container>
+        <Tabs
+          value={this.state.value}
+          onChange={this.handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+        >
+          <Tab label="Item One" />
+          <Tab label="Item Two" />
+          <Tab label="Item Three" />
+        </Tabs>
+        {value === 0 && <TabContainer>Item One</TabContainer>}
+        {value === 1 && <TabContainer>Item Two</TabContainer>}
+        {value === 2 && <TabContainer>Item Three</TabContainer>}
+      </Container>
+    );
+  }
+}
+
+export default withStyles(styles)(SearchPage);
+
+/*
 SearchPage.propTypes = {
   followingUser: PropTypes.object.isRequired,
 };
+*/
 
+/*
 export default lifecycle({
   componentDidMount() {
     // フォローしているユーザの取得
@@ -42,3 +113,4 @@ export default lifecycle({
     this.props.fetchAndSetFollower();
   },
 })(SearchPage);
+*/
