2efcae230b4b6776ee4c1da383f67da9715874d9
Kentaro Sakae <musamusa0607@gmail.com>
2018-07-08 21:18:23 +0900

    MyPageの準備

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import login from './login';
 import search from './search';
+import mypage from './mypage';
 
 const indexReducer = combineReducers({
   login,
   search,
+  mypage,
 });
 
 export default indexReducer;
