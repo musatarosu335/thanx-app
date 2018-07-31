09771ecbc414f82c18dfa16c7768b8156c01413d
Kentaro Sakae <musamusa0607@gmail.com>
2018-07-31 23:43:31 +0900

    TicketDetailsDialogのスタイル修正

--- src/components/mypage/TicketDetailsDialog.jsx
+++ src/components/mypage/TicketDetailsDialog.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
+
+const Point = styled.p`
+  font-size: 1.1rem;
+`;
 
 const TicketDetailsDialog = ({ ticket, open, handleClose }) => (
   <Dialog
@@ -17,6 +22,7 @@ const TicketDetailsDialog = ({ ticket, open, handleClose }) => (
       <DialogContentText>
         {ticket.description}
       </DialogContentText>
+      <Point>{`消費pt: ${ticket.point}pt`}</Point>
     </DialogContent>
     <DialogActions>
       <Button
