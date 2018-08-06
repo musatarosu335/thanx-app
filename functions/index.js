9b508423dc4f4f4d6038d8e1bab81ea4cc148a0d
Kentaro Sakae <musamusa0607@gmail.com>
2018-08-07 00:01:53 +0900

    サインイン後、ユーザ情報をfirestoreに登録

--- functions/index.js
+++ functions/index.js
@@ -9,18 +9,27 @@ Authentication
 // ユーザー登録後、Firestoreに登録
 exports.registerUsers = functions.auth.user().onCreate((user) => {
   const { uid } = user;
+  const db = admin.firestore();
 
   return admin.auth().getUser(uid).then((userRecord) => {
     // See the UserRecord reference doc for the contents of userRecord.
     console.log(userRecord.toJSON()); // eslint-disable-line no-console
-    const fullName = userRecord.displayName || 'Anonymous';
-    console.log(fullName);
-    /*
-    return admin.database().ref('messages').push({
-      name: 'Firebase Bot',
-      text: `${fullName} signed in for the first time! Welcome!`
-    });
-    */
+    const { email } = userRecord;
+    const displayName = userRecord.displayName || 'Anonymous';
+
+    return db.collection('users').doc(uid).set({
+      user_name: displayName,
+      email,
+      partner: '',
+      point: 0,
+      create_on: new Date(),
+    })
+      .then(() => {
+        console.log('Success write user info'); // eslint-disable-line no-console
+      })
+      .catch((err) => {
+        console.log(err); // eslint-disable-line no-console
+      });
   }).catch((error) => {
     console.log(error); // eslint-disable-line no-console
   });
