6c78fece4ee732ad891bfabd9239b8cf026fe8c6
Kentaro Sakae <musamusa0607@gmail.com>
2018-08-06 23:43:33 +0900

    Cloud Functionsでサインインしたユーザの情報を取得

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions'); // Firebase Functions SDK(HTTPリクエストをトリガするために使用)
+const admin = require('firebase-admin'); // Firebase Admin SDK(Firestoreの処理及び認証をするために使用)
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+/* ==================================
+Authentication
+================================== */
+// ユーザー登録後、Firestoreに登録
+exports.registerUsers = functions.auth.user().onCreate((user) => {
+  const { uid } = user;
+
+  return admin.auth().getUser(uid).then((userRecord) => {
+    // See the UserRecord reference doc for the contents of userRecord.
+    console.log(userRecord.toJSON()); // eslint-disable-line no-console
+    const fullName = userRecord.displayName || 'Anonymous';
+    console.log(fullName);
+    /*
+    return admin.database().ref('messages').push({
+      name: 'Firebase Bot',
+      text: `${fullName} signed in for the first time! Welcome!`
+    });
+    */
+  }).catch((error) => {
+    console.log(error); // eslint-disable-line no-console
+  });
+});
