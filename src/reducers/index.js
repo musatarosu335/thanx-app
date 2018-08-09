71618689861a640ede24df77dd17c4e0243d90c5
Kentaro Sakae <musamusa0607@gmail.com>
2018-08-09 23:10:12 +0900

    認証系をFirebaseUIに移管

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-import login from './login';
 import checkPartner from './checkPartner';
 import search from './search';
 import mypage from './mypage';
 
 const indexReducer = combineReducers({
-  login,
   checkPartner,
   search,
   mypage,
