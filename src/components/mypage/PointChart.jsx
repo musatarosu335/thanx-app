69acff2b9d399c5d90c814174bd1e4ad83397cc4
Kentaro Sakae <musamusa0607@gmail.com>
2018-08-19 13:10:40 +0900

    ResponsiveContainerでグラフをレスポンシブ対応

--- src/components/mypage/PointChart.jsx
+++ src/components/mypage/PointChart.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
 
 import CustomizedDot from './CustomizedDot';
 
@@ -69,25 +78,23 @@ const Container = styled.div`
   }
 `;
 
-export default class PointChart extends React.Component {
-  render() {
-    return (
-      <Container>
-        <LineChart
-          width={600}
-          height={300}
-          data={data}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" dot={<CustomizedDot />} />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
-      </Container>
-    );
-  }
-}
+const PointChart = () => (
+  <Container>
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart
+        data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="pv" stroke="#8884d8" dot={<CustomizedDot />} />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
+    </ResponsiveContainer>
+  </Container>
+);
+
+export default PointChart;
