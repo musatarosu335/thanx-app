721e016e37642741e1d153c23e25928a70395a6e
Kentaro Sakae <musamusa0607@gmail.com>
2018-08-21 00:04:38 +0900

    習慣のポイント取得部分をfetchWeeklyPoint関数にまとめた

--- src/components/points/DaylyPointList.jsx
+++ src/components/points/DaylyPointList.jsx
@@ -1,53 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import firebase from 'firebase/app';
-import moment from 'moment';
 import _ from 'lodash';
+
+import fetchWeeklyPoint from '../../func/fetchWeeklyPoint';
 
 export default class DaylyPointList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      daylyPointList: [],
+      daylyPointList: [], // Reduxのstateとして管理する必要なし
     };
   }
 
   componentDidMount() {
     const { date } = this.props;
-    const db = firebase.firestore();
-    const { currentUser } = firebase.auth();
-    const receivedPointRef = db.collection(`users/${currentUser.uid}/point`);
-    const today = new Date();
-    today.setDate(today.getDate() - 1); // 要調整
-    const lastWeek = new Date();
-    lastWeek.setDate(lastWeek.getDate() - 8); // 要調整
-    const receivedPointList = [];
 
-    receivedPointRef.where('sent_time', '>=', lastWeek).where('sent_time', '<', today) // 要調整
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          const sentTime = doc.data().sent_time.seconds;
-          const formattedSentTime = moment.unix(sentTime).format('MM-DD');
-          const modData = {
-            ...doc.data(),
-            formattedSentTime,
-          };
-          receivedPointList.push(modData);
-        });
-        this.setState({
-          daylyPointList: _.filter(receivedPointList, ['formattedSentTime', date]),
-        });
-      })
-      .catch((err) => {
-        console.log(err); // eslint-disable-line no-console
+    fetchWeeklyPoint().then((receivedPointList) => {
+      this.setState({
+        daylyPointList: _.filter(receivedPointList, ['formattedSentTime', date]),
       });
+    });
   }
 
   render() {
     if (this.state.daylyPointList.length === 0) {
       return (
-        <div>Now Loading...</div>
+        <div>ポイントが無いか、日付けを参照できません。</div>
       );
     }
     return (
--- src/containers/mypage/PointChart.js
+++ src/containers/mypage/PointChart.js
@@ -1,45 +1,21 @@
-import firebase from 'firebase/app';
 import { connect } from 'react-redux';
-import moment from 'moment';
 import PointChart from '../../components/mypage/PointChart';
 import { setReceivedPointList, setDaylyPointList } from '../../actions/mypage';
 
 import totalPointPerDay from '../../func/aggregatePoint';
+import fetchWeeklyPoint from '../../func/fetchWeeklyPoint';
 
 const mapStateToProps = ({ mypage }) => ({
   daylyPointList: mypage.daylyPointList,
 });
 
 const mapDispatchToProps = dispatch => ({
-  // 一週間分のポイント情報を取得
+  // 一週間分のポイント情報を取得 + 日付けごとのポイントを合計
   fetchReceivedPointList() {
-    const db = firebase.firestore();
-    const { currentUser } = firebase.auth();
-    const receivedPointRef = db.collection(`users/${currentUser.uid}/point`);
-    const today = new Date();
-    today.setDate(today.getDate() - 1); // 要調整
-    const lastWeek = new Date();
-    lastWeek.setDate(lastWeek.getDate() - 8); // 要調整
-    const receivedPointList = [];
-
-    receivedPointRef.where('sent_time', '>=', lastWeek).where('sent_time', '<', today) // 要調整
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          const sentTime = doc.data().sent_time.seconds;
-          const formattedSentTime = moment.unix(sentTime).format('MM-DD');
-          const modData = {
-            ...doc.data(),
-            formattedSentTime,
-          };
-          receivedPointList.push(modData);
-        });
-        dispatch(setReceivedPointList(receivedPointList));
-        dispatch(setDaylyPointList(totalPointPerDay(receivedPointList)));
-      })
-      .catch((err) => {
-        console.log(err); // eslint-disable-line no-console
-      });
+    fetchWeeklyPoint().then((receivedPointList) => {
+      dispatch(setReceivedPointList(receivedPointList));
+      dispatch(setDaylyPointList(totalPointPerDay(receivedPointList)));
+    });
   },
 });
 
