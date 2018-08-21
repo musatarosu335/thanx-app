44fe9a926254af19d47fa68aa94a4fe991d51499
Kentaro Sakae <musamusa0607@gmail.com>
2018-08-21 23:04:25 +0900

    /points/:date/ではurlパラメータの値からFirestoreのデータを検索

--- src/components/points/DaylyPointList.jsx
+++ src/components/points/DaylyPointList.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
+import firebase from 'firebase/app';
 
-import fetchWeeklyPoint from '../../func/fetchWeeklyPoint';
+import Alert from '../common/Alert';
 import DaylyPoint from './DaylyPoint';
 
 export default class DaylyPointList extends React.Component {
@@ -15,18 +15,32 @@ export default class DaylyPointList extends React.Component {
 
   componentDidMount() {
     const { date } = this.props;
+    const db = firebase.firestore();
+    const { currentUser } = firebase.auth();
+    const receivedPointRef = db.collection(`users/${currentUser.uid}/point`);
+    const targetDate = new Date(`${date} 00:00:00`); // urlパラメータから検索日を作成
+    const endOfTargetDate = new Date(`${date} 23:59:59`); // urlパラメータから検索日を作成(1日の終わり)
+    const daylyPointList = [];
 
-    fetchWeeklyPoint().then((receivedPointList) => {
-      this.setState({
-        daylyPointList: _.filter(receivedPointList, ['formattedSentTime', date]),
+    receivedPointRef.where('sent_time', '>=', targetDate).where('sent_time', '<=', endOfTargetDate)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          daylyPointList.push(doc.data());
+        });
+        this.setState({
+          daylyPointList,
+        });
+      })
+      .catch((err) => {
+        console.log(err); // eslint-disable-line no-console
       });
-    });
   }
 
   render() {
     if (this.state.daylyPointList.length === 0) {
       return (
-        <div>ポイントが無いか、日付けを参照できません。</div>
+        <Alert>ポイントが無いか、日付けを参照できません。</Alert>
       );
     }
     return (
