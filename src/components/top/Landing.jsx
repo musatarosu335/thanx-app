d33741706daeca080820463c37c40b624efc4228
Kentaro Sakae <musamusa0607@gmail.com>
2018-09-01 18:29:26 +0900

    Landing仮配置

--- src/components/top/Landing.jsx
+++ src/components/top/Landing.jsx
@@ -3,13 +3,39 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const Container = styled.div`
-  background-color: #ddd;
+
+`;
+
+const TitleArea = styled.div`
+  background-color: #00ad9a;
+`;
+
+const CharacteristicArea = styled.div`
+  background-color: #FFF;
+`;
+
+const HowToUseArea = styled.div`
+  background-color: #FAFAFA;
+`;
+
+const StartArea = styled.div`
+  background-color: #FFF;
 `;
 
 const Landing = () => (
   <Container>
-    <h2>Landing Page</h2>
-    <Link to="/mypage">マイページ</Link>
+    <TitleArea>
+      <Link to="/mypage">マイページ</Link>
+    </TitleArea>
+    <CharacteristicArea>
+      特徴
+    </CharacteristicArea>
+    <HowToUseArea>
+      使い方
+    </HowToUseArea>
+    <StartArea>
+      始めよう
+    </StartArea>
   </Container>
 );
 
