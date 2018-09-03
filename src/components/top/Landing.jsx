638acb76fc8f964ebf9c56c7ba6ff5af1809a737
Kentaro Sakae <musamusa0607@gmail.com>
2018-09-03 22:26:58 +0900

    TitleArea作成

--- src/components/top/Landing.jsx
+++ src/components/top/Landing.jsx
@@ -1,31 +1,66 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
 
 const Container = styled.div`
 
 `;
 
 const TitleArea = styled.div`
-  background-color: #00ad9a;
+  background: #22c1c3; /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #22c1c3, #fdbb2d); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #22c1c3, #fdbb2d); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 42px 0;
+`;
+
+const Logo = styled.img`
+  width: 80%;
+  max-width: 400px;
 `;
 
 const CharacteristicArea = styled.div`
   background-color: #FFF;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 const HowToUseArea = styled.div`
   background-color: #FAFAFA;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 const StartArea = styled.div`
   background-color: #FFF;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
-const Landing = () => (
+const Landing = ({ classes }) => (
   <Container>
     <TitleArea>
-      <Link to="/mypage">マイページ</Link>
+      <Logo alt="" src="/assets/logo/logo.png" />
+      <Link to="/mypage" style={{ textDecoration: 'none' }}>
+        <Button variant="contained" color="secondary" className={classes.button}>
+          さっそく始める
+        </Button>
+      </Link>
     </TitleArea>
     <CharacteristicArea>
       特徴
@@ -39,4 +74,8 @@ const Landing = () => (
   </Container>
 );
 
-export default Landing;
+Landing.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Landing);
