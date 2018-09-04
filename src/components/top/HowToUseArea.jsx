2bca4b33601eb2802361adb5aefa64b1d1026c9d
Kentaro Sakae <musamusa0607@gmail.com>
2018-09-04 14:38:12 +0900

    HowToUseAreaをスタイリング

--- src/components/top/HowToUseArea.jsx
+++ src/components/top/HowToUseArea.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  background-color: #FFF;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -12,6 +11,13 @@ const Container = styled.div`
 
 const Title = styled.h2`
   color: #333;
+`;
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 36px;
 `;
 
 const SubTitle = styled.div`
@@ -34,47 +40,67 @@ const Description = styled.p`
   font-size: 0.9rem;
 `;
 
-const Image = styled.img`
-  width: 40%;
+const CoupleIcon = styled.img`
+  width: 100px;
+  max-width: 200px;
+`;
+
+const SendIcon = styled.img`
+  width: 100px;
+  max-width: 200px;
+`;
+
+const TicketIcon = styled.img`
+  width: 100px;
+  max-width: 200px;
+`;
+
+const GraphIcon = styled.img`
+  width: 120px;
   max-width: 200px;
 `;
 
 const HowToUseArea = () => (
   <Container>
     <Title>thanxの使い方</Title>
-    <SubTitle>1. パートナーを見つけよう</SubTitle>
-    <Description>
-      ログインするとパートナー検索ができるようになります。
-      パートナーとペアが成立するとマイページへ移動できます。
-    </Description>
-    <Image src="/assets/icon/couple.svg" />
-    <SubTitle>2. ポイントを送り合おう</SubTitle>
-    <Description>
-      料理や洗濯など、日々の感謝をポイントとしてパートナーに送りましょう。
-      一緒にメッセージも添えてあげると感謝の気持ちがより伝わります。
-    </Description>
-    <Image
-      src="/assets/icon/send_screen.png"
-      style={{
-        border: '1px solid rgba(180, 180, 180, 0.48)',
-        borderRadius: 7,
-      }}
-    />
-    <SubTitle>3. チケットと交換しよう</SubTitle>
-    <Description>
-      日々のポイントは貯めることができます。
-      貯めたポイントはチケットと交換できます。
-    </Description>
-    <Image src="/assets/icon/ticket.svg" />
-    <SubTitle>4. チケットを実行しよう</SubTitle>
-    <Description>
-        交換したチケットをパートナーに実行してもらいましょう。
-      （チケットは随時追加予定です）
-    </Description>
-    <Image src="/assets/icon/ticket.svg" />
-    <SubTitle>5. ポイントを確認しよう</SubTitle>
-    <Description>日々のポイント獲得状況はマイページから確認できます。</Description>
-    <Image src="/assets/icon/graph.svg" />
+    <Section>
+      <SubTitle>1. パートナーを見つけよう</SubTitle>
+      <Description>
+        ログインするとパートナー検索ができるようになります。
+        パートナーとペアが成立するとマイページへ移動できます。
+      </Description>
+      <CoupleIcon src="/assets/icon/couple.svg" />
+    </Section>
+    <Section>
+      <SubTitle>2. ポイントを送り合おう</SubTitle>
+      <Description>
+        料理や洗濯など、日々の感謝をポイントとしてパートナーに送りましょう。
+        一緒にメッセージも添えてあげると感謝の気持ちがより伝わります。
+      </Description>
+      <SendIcon src="/assets/icon/send.png" />
+    </Section>
+    <Section>
+      <SubTitle>3. チケットと交換しよう</SubTitle>
+      <Description>
+        日々のポイントは貯めることができます。
+        貯めたポイントはチケットと交換できます。
+      </Description>
+      <TicketIcon src="/assets/icon/ticket.svg" />
+    </Section>
+    <Section>
+      <SubTitle>4. チケットを実行しよう</SubTitle>
+      <Description>
+          交換したチケットをパートナーに実行してもらいましょう。
+          お互いのチケット状況はマイページから確認できます。
+        （チケットは随時追加予定です）
+      </Description>
+      <TicketIcon src="/assets/icon/do.svg" />
+    </Section>
+    <Section>
+      <SubTitle>5. ポイントを確認しよう</SubTitle>
+      <Description>日々のポイント獲得状況はマイページから確認できます。</Description>
+      <GraphIcon src="/assets/icon/graph.png" />
+    </Section>
   </Container>
 );
 
