2494b15d5b973cf7316d6d2db1b189378da34df2
Kentaro Sakae <musamusa0607@gmail.com>
2018-09-04 14:42:02 +0900

    グラデーション修正

--- src/components/top/TitleArea.jsx
+++ src/components/top/TitleArea.jsx
@@ -14,9 +14,10 @@ const styles = theme => ({
 });
 
 const Container = styled.div`
-  background: #22c1c3; /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #22c1c3, #fdbb2d); /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to right, #22c1c3, #fdbb2d); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: #00C9FF;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #92FE9D, #00C9FF);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #92FE9D, #00C9FF); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
   display: flex;
   flex-direction: column;
   justify-content: center;
